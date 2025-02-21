5cd0dce7a24c3c11696be801c3b62733b30ada34
tathagatsen <114797100+tathagatsen@users.noreply.github.com>
2025-02-21 22:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from fractions import Fraction
 app = Flask(__name__)
 
 # Load the reference file into memory at startup
-REFERENCE_FILE_PATH = os.path.join(os.getcwd(), "public", "BOQ.xlsx")
+REFERENCE_FILE_PATH = os.path.join(os.getcwd(), "public", "COSTING SHEET MASTER NEW.xlsx")
 
 REFERENCE_WORKBOOK = openpyxl.load_workbook(REFERENCE_FILE_PATH, data_only=True)
 
